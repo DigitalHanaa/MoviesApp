3f6ab77b200d3763178c82d9ed7889ec97e1f326
byhanaa <digitalhanaa@gmail.com>
2021-02-12 22:36:46 +0300

    part 2 of the app - films

---
+++ src/Fave.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Fave extends Component {
+
+    constructor(props) {
+        super(props)
+        // super is Component class
+        this.state = {
+            isFave: false,
+        }
+    }
+
+    handleClick(e){
+        /*
+            The stopPropagation() method prevents propagation of the same event from being called.
+            Propagation means bubbling up to parent elements or capturing down to child elements.
+        */
+        
+        this.setState({
+            // !this.state.isFave = which !falue = true
+            //isFave: !this.state.isFave,
+            isFave: !this.state.isFave,
+        })
+        console.log("handling Fave click!", this.state.isFave);
+        e.stopPropagation();
+        
+    }
+
+    render() {
+        const isFaveClass = (this.state.isFave) ? 'remove_from_queue' : 'add_to_queue'
+        return (
+            <div className={`film-row-fave ${isFaveClass}`} onClick={(e) => this.handleClick(e)}>
+                <p className="material-icons">{isFaveClass}</p>
+            </div>
+        )
+    }
+}
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import FilmPoster from './FilmPoster';
+import Fave from './Fave';
 
 export default class FilmRow extends Component {
+
+    handleDetailsClick(film){
+        console.log("Fetching details for " , film.title)
+    }
+
     render() {
         
         return (
-            <div className="film-row">
+            <div className="film-row" onClick={() => this.handleDetailsClick(this.props.film)}>
                 <FilmPoster film={this.props.film}/>
                 <div className="film-summary">
+                    <Fave/>
                     <h1>{this.props.film.title}</h1>
                     <p>{new Date(this.props.film.release_date).getFullYear()}</p>
                 </div>
